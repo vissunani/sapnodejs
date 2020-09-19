849191952f3030397f69d1d3b8cf49233a298327
Viswanath Matukumalli <170030796@kluniversity.in>
2020-09-19 19:36:05 +0530

    Second  commit

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.write('Hello World!'); 
-    res.end(); 
-  }).listen(8080); 
+const express=require('express')
+
+const app = express();
+
+app.get('/',function(req,res)
+{
+    res.send("It is home page");
+})
+
+app.get('/:name',function(req,res)
+{
+    const name = req.params.name;
+    res.send("It is "+name+" page");
+})
+
+
+
+
+app.listen(9000,function(req,res)
+{
+    console.log("Hello Evryone");
+}
+);
